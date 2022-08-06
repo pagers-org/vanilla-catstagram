577e2b66c185c09279c704c1fdd506f22dacd120
InSeong-So <goflvhxj2547@gmail.com>
2022-08-06 21:29:17 +0900

    🎆Style: 불필요 함수 제거

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 import '../assets/scss/index.scss';
 
-const setCatImage = async () => {
-  const $container = document.querySelector('.container');
+const $container = document.querySelector('.container');
 
-  for (let i = 10; i > 0; i--) {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?size=full');
-    const data = await response.json();
-    const { id, url } = data[0];
-    const $pin = /* html */`
-    <div class="pin">
-      <div class="button-wrapper">
-        <div class="anim-icon anim-icon-md heart">
-          <input type="checkbox" id="cat-${id}" />
-          <label for="cat-${id}"></label>
-        </div>
+for (let i = 10; i > 0; i--) {
+  const response = await fetch('https://api.thecatapi.com/v1/images/search?size=full');
+  const data = await response.json();
+  const { id, url } = data[0];
+  const $pin = /* html */`
+  <div class="pin">
+    <div class="button-wrapper">
+      <div class="anim-icon anim-icon-md heart">
+        <input type="checkbox" id="cat-${id}" />
+        <label for="cat-${id}"></label>
       </div>
-      <img src="${url}" />
-    </div>`;
-    $container.insertAdjacentHTML('afterbegin', $pin);
-  }
+    </div>
+    <img src="${url}" />
+  </div>`;
+  $container.insertAdjacentHTML('afterbegin', $pin);
 }
-
-setCatImage();
